7ca4a3c55ce9856b3118a82fb569cdecfe2f5225
Malga90 <savemeebarry@gmail.com>
2019-06-15 22:16:19 +0200

    navigation styled

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import _ from "lodash";
 import "./style.scss";
 import Icon from "./icon.png";
 
-function component() {
-  const element = document.createElement("div");
+// function component() {
+//   const element = document.createElement("div");
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
+//   // Lodash, currently included via a script, is required for this line to work
+//   element.innerHTML = _.join(["Hello", "webpack"], " ");
+//   element.classList.add("hello");
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
+//   const myIcon = new Image();
+//   myIcon.src = Icon;
 
-  element.appendChild(myIcon);
+//   element.appendChild(myIcon);
 
-  return element;
-}
+//   return element;
+// }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
